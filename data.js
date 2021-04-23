72347a989934372c5f8f66523418da1a501ab19b
chris westerduin <chriswesterduin@outlook.com>
2021-04-23 23:04:35 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,106 @@
+let game = {
+  id: "6082f217db6b0dc80800c436",
+  players: [
+    { id: "", name: "player1" },
+    { id: "", name: "player2" },
+    { id: "", name: "player3" },
+  ],
+  questions: [
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "In World War II, Hawker Typhoons served in the Pacific theater.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+      player_answers: [
+          {id: "", correct: "True"},
+          {id: "", correct: "True"},
+          {id: "", correct: "False"}
+      ]
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "Sargon II, a king of the Neo-Assyrian Empire, was a direct descendant of Sargon of Akkad.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "Assyrian king Sennacherib&#039;s destruction of Babylon in 689 BCE was viewed as a triumph by other Assyrian citizens.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "&quot;I disapprove of what you say, but I will defend to the death your right to say it&quot; is a quote from French philosopher Voltaire.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "Theodore Roosevelt Jr. was the only General involved in the initial assault on D-Day.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to raise moral questions about banning drugs.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "United States President Ronald Reagan was the first president to appoint a woman to the Supreme Court. ",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question:
+        "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "medium",
+      question: "Franz Joseph I was the last emperor of Austria.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+  ],
+    results: [
+    ]
+};
