3a26252419ca706432a0e54e52815dbbe439764d
chris westerduin <chriswesterduin@outlook.com>
2021-04-25 12:49:05 +0100

    add current count in room

--- client/src/pages/ChatRoom/index.js
+++ client/src/pages/ChatRoom/index.js
@@ -5,29 +5,62 @@ import { Chat } from '../../components'
 import { addMessage } from '../../actions'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const serverEndpoint = "http://localhost:3000";
 
 function ChatRoom() {
   const { id } = useParams()
   const [socket, setSocket] = useState(null);
+
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState()
+  const [room, setRoom] = useState()
+
+  const [count, setCount] = useState(0)
+
   const messages = useSelector(state => state)
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const socket = io(serverEndpoint);
-    setSocket({ socket });
-    socket.emit('create', id);
-    socket.on('admin-message', msg => {
-      dispatch(addMessage('admin', msg))
-    })
-    socket.on('incoming-message', ({ username, message }) => {
-      dispatch(addMessage(username, message))
-    })
-    return () => {
-        socket.disconnect()
+    async function validRoom() {
+      try {
+        setLoading(true)
+        let { data } = await axios.get(`http://localhost:3000/games/${id}`);
+        setLoading(false)
+        setRoom(data)
+      } catch (err) {
+        console.warn(err);
+        setLoading(false)
+        setError(err);
+      }
     }
+    validRoom();
   }, []);
+
+  useEffect(() => {
+    if (room) {
+      const socket = io(serverEndpoint);
+      setSocket({ socket });
+      socket.emit("create", id);
+
+      socket.on("admin-message", (msg) => {
+        dispatch(addMessage("admin", msg));
+      });
+
+      socket.on("count", (count) => {
+        setCount(count)
+      })
+
+      socket.on("incoming-message", ({ username, message }) => {
+        dispatch(addMessage(username, message));
+      });
+      return () => {
+        socket.disconnect();
+      };
+    } else {
+    }
+  }, [room]);
 
 
   const sendMessage = (event, { message, setMessage }) => {
@@ -39,10 +72,13 @@ function ChatRoom() {
 
   return (
     <>
-    <div id="App">Room: {id}</div>
-    <Chat messages={messages} sendMessage={sendMessage}/>
+      <div id="App">Room: {id}</div><span>users: {count}</span>
+{      loading ? (<div>loading...</div>) : error ? (<div>{JSON.stringify(error)}</div>
+      ) : (
+      <Chat messages={messages} sendMessage={sendMessage} />
+      )}
     </>
-  )
+  );
 }
 
 export default ChatRoom
